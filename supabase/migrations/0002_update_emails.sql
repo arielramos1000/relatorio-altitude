7c7143f70a2c644ef378072edcceb43a0e66e63b
arielramos1000 <ariel@barninvest.com.br>
2026-05-09 14:53:50 -0300

    chore: atualiza emails dos destinatários

---
+++ supabase/migrations/0002_update_emails.sql
@@ -0,0 +1,3 @@
+UPDATE people SET email = 'adolfo@barninvest.com.br' WHERE name = 'Adolfo';
+UPDATE people SET email = 'ariel@barninvest.com.br'  WHERE name = 'Ariel';
+UPDATE people SET email = 'sayeg@barninvest.com.br'  WHERE name = 'Sergio';
